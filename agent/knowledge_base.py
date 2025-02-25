dab7add0652d8ca9b6e7bf032fb4076d47a2d072
Dylan Holland <dylaneholland@gmail.com>
2025-02-25 15:27:01 -0800

    debugging

--- agent/knowledge_base.py
+++ agent/knowledge_base.py
@@ -1,8 +1,10 @@
-from agent.helpers import get_agent, save_user
+from agent.helpers import get_agent, get_user_from_db, save_user
 
 # print(get_agent())
 
-save_user({
-    "phone_number": "+6187516231",
-    "name": "Dylan"
-})
+# save_user({
+#     "phone_number": "+6187516231",
+#     "name": "Dylan"
+# })
+
+print(get_user_from_db("+6187516231"))
