a716a7635cb3d66bcabfa78e7f1ac66c572747a6
Dylan Holland <dylaneholland@gmail.com>
2025-02-25 14:57:19 -0800

    Adding init route for twilio initialization

--- agent/__main__.py
+++ agent/__main__.py
@@ -1,8 +1,25 @@
+from typing import Literal, Mapping
 from fastapi import FastAPI, Request
 
 from agent.helpers import get_note_from_db, save_note, search_from_query
 
 app = FastAPI()
+
+# @app.post("/agent/search")
+# async def search(request: Request) -> dict[str, str]:
+#     request_body = await request.json()
+#     result = search_from_query(request_body['search_query'])
+#     return {
+#         "result": result
+#     }
+
+# @app.get("/agent/get-note")
+# async def get_note(request: Request) -> dict[str, str]:
+#     note = get_note_from_db()
+#     print("got note:", note)
+#     return {
+#         "note": note
+#     }
 
 @app.get("/")
 def read_root() -> dict[str, str]:
@@ -16,14 +33,6 @@ async def take_note(request: Request) -> dict[str, str]:
     else:
         return {"status": "error"}
 
-@app.post("/agent/search")
-async def search(request: Request) -> dict[str, str]:
-    request_body = await request.json()
-    result = search_from_query(request_body['search_query'])
-    return {
-        "result": result
-    }
-
 @app.post("/agent/commit")
 async def search(request: Request) -> dict[str, str]:
     request_body = await request.json()
@@ -31,22 +40,23 @@ async def search(request: Request) -> dict[str, str]:
     # result = search_from_query(request_body['search_query'])
     return {}
 
-@app.get("/agent/get-note")
-async def get_note(request: Request) -> dict[str, str]:
-    note = get_note_from_db()
-    print("got note:", note)
-    return {
-        "note": note
-    }
-
 @app.post("/agent/init")
-async def init(request: Request) -> dict[str, dict[str, str]]:
+async def init(request: Request) -> Any: # dict[Literal["dynamic_variables", "conversation_config_override"], dict[str, str | dict[str, dict[str, str | dict[str, str]]]]]:
     request_body = await request.json()
-    
+
     print(request_body)
     
     return {
         "dynamic_variables": {
             "name": "Dylan",
+        },
+        "conversation_config_override": {
+            "agent": {
+                "prompt": {
+                    "prompt": "The customer's bank account balance is $100. They are based in San Francisco."
+                },
+                "first_message": "Hi, Dylan, how can I help you today?",
+                "language": "en"
+            }
         }
     }
